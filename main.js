d3feb4c3254d753e2be4c3e1533714e45ef8c28f
anushka rawat <138669169+Anushkrawat16@users.noreply.github.com>
2024-04-11 13:55:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+var typed = new Typed(".text", {
+    strings: ["Software Developer", "software Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+});
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab")
+}
+
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+function openmenu() {
+    sidemenu.style.right = "-200px";
+}
+
+
+const scriptURL = https//script.google.com/macros/s/AKfycbz5JovzE5dIG0zAqH4y2-GDQuPmTP4wxnf0CmdT8kF1OHcJgk5jHYVZ8KjCVijUjbb_yQ/exec
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message sent sucessfully"
+            setTimeout(function () {
+                msg.innerHTML = ""
+            }, 5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
