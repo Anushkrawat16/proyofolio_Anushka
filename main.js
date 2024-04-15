4df439b1aeadd6adcf63dea8f78d96c947cf69c5
anushka rawat <138669169+Anushkrawat16@users.noreply.github.com>
2024-04-15 11:58:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Software Developer", "software Engineer"],
+    strings: ["Software Developer", "Web developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
